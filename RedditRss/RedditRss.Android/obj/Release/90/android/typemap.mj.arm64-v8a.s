5613f42e0e5cc7a11ff7c55ebf7b62ee273561c9
psy <brutalpsy89@gmail.com>
2020-01-19 19:08:39 +0100

    changed debug configuration.

---
+++ RedditRss/RedditRss.Android/obj/Release/90/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 5627ad0a1adc050214b54c7c6ff3909c7393fa23 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	863
+	/* entry-length */
+	.word	232
+	/* value-offset */
+	.word	130
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 200217
+	.include	"typemap.mj.inc"
